f537db7c23bcb262753fc57a23577cd717a56dbb
samjolley <sam@jolleydigital.com>
2016-07-19 13:59:30 -0400

    ex7.rb

---
+++ lrthw/ex7.rb
@@ -0,0 +1,23 @@
+# puts = 'put string'. Adds the line of code and also adds a new line after executing.
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everwhere that mary went."
+puts "." * 10 # what'd that do?
+
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line. What's it do?
+# Prefixes "CheeseBurger" before '$'
+print end1 + end2 + end3 + end4 + end5 + end6 + end7 + end8 + end9 + end10 + end11 + end12
