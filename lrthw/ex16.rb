9c7d63b11d7a3254838539c025cb5d9ec95db050
samjolley <sam@jolleydigital.com>
2016-07-20 19:16:14 -0400

    Tidy ex16.rb

--- lrthw/ex16.rb
+++ lrthw/ex16.rb
@@ -14,12 +14,9 @@ target.truncate(0)
 
 puts "Now I'm going to ask you for three lines."
 
-print "line 1: "
-line1 = $stdin.gets.chomp
-print "line 2: "
-line2 = $stdin.gets.chomp
-print "line 3: "
-line3 = $stdin.gets.chomp
+print "line 1: "; line1 = $stdin.gets.chomp
+print "line 2: "; line2 = $stdin.gets.chomp
+print "line 3: "; line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
 
