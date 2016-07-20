4cc4d1446bdec71692e59d31b58fcf96bd171e77
samjolley <sam@jolleydigital.com>
2016-07-20 09:11:22 -0400

    ex9.rb

---
+++ lrthw/ex9.rb
@@ -0,0 +1,17 @@
+# Here's some new strange stuff. Remember, type it exactly. 
+
+# Define variables and assign values
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Display days and months
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+# Display many lines with the three double-quotes
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
