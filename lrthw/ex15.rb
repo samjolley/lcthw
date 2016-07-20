e1428a3b749bebf615b6a9a96b3b97b43503c681
samjolley <sam@jolleydigital.com>
2016-07-20 18:56:35 -0400

    modify ex15.rb

--- lrthw/ex15.rb
+++ lrthw/ex15.rb
@@ -10,6 +10,15 @@ puts "Here's your file #{filename}:"
 # Print the contents of the .txt file
 print txt.read
 
+txt.close
 # Print a message for the user
 print "Type the filename again: "
+
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
+
+txt_again.close
 
