7bdc9b234d6bd4f48ce37e0103631c24b04292ca
samjolley <sam@jolleydigital.com>
2016-07-20 09:20:35 -0400

    ex10.rb

---
+++ lrthw/ex10.rb
@@ -0,0 +1,17 @@
+# Define variables with various escape quote examples
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+# Display it all
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
