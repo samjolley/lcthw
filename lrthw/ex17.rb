1d2e84d1a3594d9ee477a0b9bb915e8b787bb379
samjolley <sam@jolleydigital.com>
2016-07-21 11:00:43 -0400

    Remove 'Does the output file exist?'

--- lrthw/ex17.rb
+++ lrthw/ex17.rb
@@ -4,8 +4,6 @@ puts "Copying from #{from_file} to #{to_file}"
 
 in_file = open(from_file); indata = in_file.read
 
-puts "Does the output file exist? #{File.exist?(to_file)}"
-
 out_file = open(to_file, 'w'); out_file.write(indata)
 
 puts "Alright, all done."
