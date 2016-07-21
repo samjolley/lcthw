42beb2eaeafa5211b28a7d5c1eee48b2fe0b27e2
samjolley <sam@jolleydigital.com>
2016-07-21 10:56:16 -0400

    Remove ask user feature

--- lrthw/ex17.rb
+++ lrthw/ex17.rb
@@ -1,4 +1,4 @@
-from_file, to_file - ARGV
+from_file, to_file = ARGV
 
 puts "Copying from #{from_file} to #{to_file}"
 
@@ -7,13 +7,9 @@ in_file = open(from_file); indata = in_file.read
 puts "The input file is #{indata.length} bytes long"
 
 puts "Does the output file exist? #{File.exist?(to_file)}"
-puts "Ready, hit REATURN to continue, CTRL-C to abort."
-$stdin.gets
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+out_file = open(to_file, 'w'); out_file.write(indata)
 
 puts "Alright, all done."
 
-out_file.close
-in_file.close
+out_file.close; in_file.close
